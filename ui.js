62047432a45d2ed7e82808e2115c45f24531342e
PixelZerg <pixelzerg@gmail.com>
2019-03-19 20:18:19 +0000

    styling

--- ui.js
+++ ui.js
@@ -3,5 +3,9 @@ $('.matrix .lbrac').css("opacity","1");
 $('.matrix .rbrac').css("opacity","1");
 
 $(window).on('resize', function(){
-    $('.matrix .rbrac').css("left",$('.matrix').outerWidth()-6);
+    var top = 25+$('.matrix').prevAll().outerHeight(true);
+    $('.matrix .lbrac').css("top", top);
+    $('.matrix .rbrac').css("top", top);
+
+    $('.matrix .rbrac').css("left", $('.matrix').outerWidth()-6);
 }).resize();
